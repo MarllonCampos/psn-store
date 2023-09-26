7360747b8b859551f91ee78d570a29a61ab83956
Marllon Campos <52542050+MarllonCampos@users.noreply.github.com>
2023-09-26 16:59:05 -0300

    feat: make the card component the principal component

--- src/app/components/card/components/card-price-tag/card-price-tag.component.ts
+++ src/app/components/card/components/card-price-tag/card-price-tag.component.ts
@@ -7,7 +7,7 @@ import { Component, Input } from '@angular/core';
 })
 export class CardPriceTagComponent {
   @Input()
-  gameType: string = 'Digital PS4';
+  gameType: string = '';
   @Input()
-  gamePrice: string = 'R$ 399,90';
+  gamePrice: string = '';
 }
