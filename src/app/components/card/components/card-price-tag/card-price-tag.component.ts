1c2f27e8ca90e7ba251c877519a26d35346868e5
Marllon Campos <52542050+MarllonCampos@users.noreply.github.com>
2023-09-26 16:47:11 -0300

    feat: styling card price-tag and make it receive some directives

--- src/app/components/card/components/card-price-tag/card-price-tag.component.ts
+++ src/app/components/card/components/card-price-tag/card-price-tag.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card-price-tag',
   templateUrl: './card-price-tag.component.html',
-  styleUrls: ['./card-price-tag.component.scss']
+  styleUrls: ['./card-price-tag.component.scss'],
 })
 export class CardPriceTagComponent {
-
+  @Input()
+  gameType: string = 'Digital PS4';
+  @Input()
+  gamePrice: string = 'R$ 399,90';
 }
